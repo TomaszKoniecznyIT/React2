3b6e12551cce72c6a3b19be3a9d678109b81fa82
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-04 16:18:52 +0100

    corrections

--- 01-starting-setup/src/components/Concept.js
+++ 01-starting-setup/src/components/Concept.js
@@ -1,14 +1,10 @@
 const Concept = (props) => {
   return (
-    <div>
-      <ul id="concepts">
-        <li className="concept">
-          <img src={props.image} alt={props.title} />
-          <h2>{props.title}</h2>
-          <p>{props.description}</p>
-        </li>
-      </ul>
-    </div>
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
   );
 };
 
--- 01-starting-setup/src/components/Concepts.js
+++ 01-starting-setup/src/components/Concepts.js
@@ -2,25 +2,23 @@ import Concpet from "./Concept";
 
 const Concepts = (props) => {
   return (
-    <div>
-      <div>
-        <Concpet
-          image={props.items[0].image}
-          title={props.items[0].title}
-          description={props.items[0].description}
-        />
-        <Concpet
-          image={props.items[1].image}
-          title={props.items[1].title}
-          description={props.items[1].description}
-        />
-        <Concpet
-          image={props.items[2].image}
-          title={props.items[2].title}
-          description={props.items[2].description}
-        />
-      </div>
-    </div>
+    <ul id="concepts">
+      <Concpet
+        image={props.items[0].image}
+        title={props.items[0].title}
+        description={props.items[0].description}
+      />
+      <Concpet
+        image={props.items[1].image}
+        title={props.items[1].title}
+        description={props.items[1].description}
+      />
+      <Concpet
+        image={props.items[2].image}
+        title={props.items[2].title}
+        description={props.items[2].description}
+      />
+    </ul>
   );
 };
 
